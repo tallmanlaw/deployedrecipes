86f4cf28f18481d58703708ec126bc5f88e92d01
Georgie <tisha@tallmanlawyer.com>
2019-01-03 15:32:01 -0500

    Updates

---
+++ precache-manifest.b0053d79156cd995c423f6a90275409f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b27e3a23ba4ff2249515c8c01f9a0f3",
+    "url": "/deployedrecipes/static/media/cinnamon.6b27e3a2.jpg"
+  },
+  {
+    "revision": "bd3d43d2e342726091d7",
+    "url": "/deployedrecipes/static/css/main.4f741a29.chunk.css"
+  },
+  {
+    "revision": "94e939979df047f7c55d",
+    "url": "/deployedrecipes/static/js/1.94e93997.chunk.js"
+  },
+  {
+    "revision": "517c482c4bade9589954",
+    "url": "/deployedrecipes/static/js/runtime~main.517c482c.js"
+  },
+  {
+    "revision": "3ef27326d09737b5daa34fc59c052d5a",
+    "url": "/deployedrecipes/static/media/maccheese.3ef27326.jpg"
+  },
+  {
+    "revision": "5148ff7711505d19bac13a8389b59f6c",
+    "url": "/deployedrecipes/static/media/turkeycass.5148ff77.jpg"
+  },
+  {
+    "revision": "c876578936fae26df3ec3be847613868",
+    "url": "/deployedrecipes/static/media/cranberry.c8765789.jpg"
+  },
+  {
+    "revision": "9935d5dbe104be7b7e59d43fbeee1960",
+    "url": "/deployedrecipes/static/media/antslog.9935d5db.jpg"
+  },
+  {
+    "revision": "b3d5bae139046afcf29110bda88b7435",
+    "url": "/deployedrecipes/static/media/turkey.b3d5bae1.jpeg"
+  },
+  {
+    "revision": "271c6d0c0e515c46b872a5bd623fa5e4",
+    "url": "/deployedrecipes/static/media/nachos.271c6d0c.jpg"
+  },
+  {
+    "revision": "bd3d43d2e342726091d7",
+    "url": "/deployedrecipes/static/js/main.bd3d43d2.chunk.js"
+  },
+  {
+    "revision": "b52d50a725ed4a510c2541af6ed57e1d",
+    "url": "/deployedrecipes/static/media/sugarcookies.b52d50a7.jpg"
+  },
+  {
+    "revision": "948446170036ae7611dd57444695ce82",
+    "url": "/deployedrecipes/static/media/hummus.94844617.jpg"
+  },
+  {
+    "revision": "bd17f458c7a7433d06bd058ee7e55bcc",
+    "url": "/deployedrecipes/static/media/chickensoup.bd17f458.jpg"
+  },
+  {
+    "revision": "54b57415643082614868c7491e3be0d8",
+    "url": "/deployedrecipes/static/media/caramel.54b57415.jpg"
+  },
+  {
+    "revision": "dd11711919cb57e6596f10ccc3a7fec2",
+    "url": "/deployedrecipes/static/media/candied.dd117119.jpg"
+  },
+  {
+    "revision": "7cef95f96ee25e4849e54f6081eccfb9",
+    "url": "/deployedrecipes/static/media/juicepop.7cef95f9.JPG"
+  },
+  {
+    "revision": "00e7cda74b20d8e1ef0d8d6cc7810add",
+    "url": "/deployedrecipes/static/media/hotdog.00e7cda7.jpg"
+  },
+  {
+    "revision": "4ecdb990bdf05349de529f61108cfdfb",
+    "url": "/deployedrecipes/static/media/brie.4ecdb990.jpg"
+  },
+  {
+    "revision": "153f4f4ed9ef19a0268a58b0b450c034",
+    "url": "/deployedrecipes/index.html"
+  }
+];
